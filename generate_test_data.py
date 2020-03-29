b8e465248b2f9aa214078aaefa04e7e17dc4c051
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2020-03-29 08:44:30 -0500

    Changed how we generate test cases to allow building subdirectories.

--- generate_test_data.py
+++ generate_test_data.py
@@ -3,8 +3,9 @@
 import os
 root_dir = os.path.abspath('.')
 
-for _, _, files in os.walk(root_dir, topdown=True):
+for dirpath, subdirs, files in os.walk(root_dir, topdown=True):
 	with open(os.path.join(root_dir, root_dir + '-testcase.txt'), 'w') as f:
 		for file in files:
-			filename = os.path.splitext(file)[0]
+			#filename = os.path.splitext(file)[0]
+			filename = os.path.join(dirpath, file)
 			f.write(filename + '\n')
